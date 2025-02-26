2cb0bab9954996f0958c7b4954283a5a23c537cc
Guilherme Matera <guimatera@gmail.com>
2025-02-26 07:42:31 -0300

    feature: Criando página estática de Informações de Elétrica

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import GeneralInfo from "./screens/GeneralInfo/index";
+import ElectricalInfo from "./screens/ElectricalInfo/index";
 import { GlobalStyle } from "./global";
 
 export default function App() {
