abc43e6ca66cdecc6a679dd115b2778272c50191
Guilherme Matera <guimatera@gmail.com>
2025-03-01 14:55:50 -0300

    feature: Criação dos botões estáticos de baixar e apagar dados.

--- src/components/DataCard/index.tsx
+++ src/components/DataCard/index.tsx
@@ -1,5 +1,6 @@
 //Estilo
-import { StyledWrapper } from "./style";
+import { StyledWrapper, Button, Text } from "./style";
+
 
 function DataCard() {
   return (
@@ -8,7 +9,14 @@ function DataCard() {
       height={"6.25rem"}
       left={"55.5rem"}
       top={"17rem"}
-    ></StyledWrapper>
+    >
+      <Button width={'10.625rem'} height={'2.8125rem'} color={"var(--acceptColor)"} left={'2.6875rem'} top={'1.7375rem'}>
+        <Text color={'var(--white)'}>Baixar Dados</Text>
+      </Button>
+      <Button width={'10.625rem'} height={'2.8125rem'} color={"var(--alertColor)"} left={'17.875rem'} top={'1.7375rem'}>
+      <Text color={'var(--white)'}>Apagar Dados</Text>
+      </Button>
+    </StyledWrapper>
   );
 }
 
--- src/components/DataCard/style.tsx
+++ src/components/DataCard/style.tsx
@@ -16,3 +16,24 @@ export const StyledWrapper = styled(Wrapper)<Props>`
   top: ${(props) => props.top};
   border-radius: 1.5625rem;
 `;
+
+export const Button = styled.button<Props>`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  left: ${(props) => props.left};
+  top: ${(props) => props.top};
+  background-color: ${(props) => props.color};
+  border-radius: 0.3125rem;
+  position:absolute;
+  filter:drop-shadow(0rem 0.25rem 0.25rem rgba(0,0,0,0.25));
+`;
+
+export const Text = styled.h3`
+  color: ${(props) => props.color};
+  text-align: center;
+  font-size: 1.25rem;
+  font-family: var(--primaryFont);
+  font-weight: 500;
+  width: 8rem;
+  margin-top:3px;
+`
