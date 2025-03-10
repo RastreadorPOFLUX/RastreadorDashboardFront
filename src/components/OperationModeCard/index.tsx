0dd64026fdac5ab67c4e8b41b04e851a3db7f960
Guilherme Matera <guimatera@gmail.com>
2025-03-10 16:39:46 -0300

    feature: Criando a dinâmica da seleção dos botões do card de modos de operação.

--- src/components/OperationModeCard/index.tsx
+++ src/components/OperationModeCard/index.tsx
@@ -1,7 +1,31 @@
 //Estilo
+import { useState } from "react";
 import { StyledWrapper, Button, Text } from "./style";
 
 function OperationModeCard() {
+
+  const [isActivedAuto, setActivatedAuto] = useState(true);
+  const [isActiveManual, setActivatedManual] = useState(false);
+  const [isActiveHalt, setActivatedHalt] = useState(false);
+
+  const handleButtonClick1 = () => {
+    isActivedAuto == false ? setActivatedAuto(!isActivedAuto): setActivatedAuto(isActivedAuto);
+    setActivatedManual(false);
+    setActivatedHalt(false);
+  };
+  const handleButtonClick2 = () => {
+    setActivatedAuto(false);
+    isActiveManual == false ? setActivatedManual(!isActiveManual): setActivatedManual(isActiveManual);
+    setActivatedHalt(false);
+    
+  };
+  const handleButtonClick3 = () => {
+    setActivatedAuto(false);
+    setActivatedManual(false);
+    isActiveHalt == false ? setActivatedHalt(!isActiveHalt): setActivatedHalt(isActiveHalt);
+  };
+
+
   return (
     <StyledWrapper
       width={"31.375rem"}
@@ -9,20 +33,22 @@ function OperationModeCard() {
       left={"21.5625rem"}
       top={"9.4375rem"}
     >
-            <Button
-              color={"var(--primaryColor)"}
+            <Button onClick={handleButtonClick1}
+              color={isActivedAuto ? "var(--primaryColor)": "var(--secondaryColor)"}
               left={"3.0156rem"}
             >
               <Text color={"var(--white)"}>Auto</Text>
             </Button>
-            <Button
-              color={"var(--secondaryColor)"}
+            <Button 
+              onClick={handleButtonClick2}
+              color={isActiveManual ? "var(--primaryColor)": "var(--secondaryColor)"}
               left={"12.46875rem"}
             >
               <Text color={"var(--white)"}>Manual</Text>
             </Button>
-            <Button
-              color={"var(--secondaryColor)"}
+            <Button 
+              onClick={handleButtonClick3}
+              color={isActiveHalt ? "var(--primaryColor)": "var(--secondaryColor)"}
               left={"21.921875rem"}
             >
               <Text color={"var(--white)"}>Halt</Text>
