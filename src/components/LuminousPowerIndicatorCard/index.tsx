71da3e1540ecd86ee1e01df9cd758e81ede0f39d
Guilherme Matera <guimatera@gmail.com>
2025-03-18 09:56:28 -0300

    feature: criação do mostrador estático da potência do motor

--- src/components/LuminousPowerIndicatorCard/index.tsx
+++ src/components/LuminousPowerIndicatorCard/index.tsx
@@ -6,57 +6,53 @@ import { AgRadialGaugeOptions } from "ag-charts-enterprise";
 //Estilo
 import { StyledWrapper, Text } from "./style";
 
-
 function LuminousPowerIndicatorCard() {
-
   const [options, setOptions] = useState<AgRadialGaugeOptions>({
-      type: "radial-gauge",
-      value: 1000,
-      scale: {
-        min: 0,
-        max: 1300,
-        fill: '#FFFFFF',
-        label: {
-          enabled: false,
-      }
-      },
-      targets: [
-        {
-            value: 1050,
-            placement: 'outside',
-            shape: 'triangle',
-            fill: '#000000'
-        },
-      ],
-      bar: {
-        fill:  '#DD702C',
-      },
-      background: {
-        visible: false
-    },
-      cornerRadius: 99,
-      cornerMode: "container",
-      segmentation: {
-        enabled: false,
-        interval: {
-          count: 4,
-        },
-        spacing: 2,
-      },
-      startAngle:0,
-      endAngle: 0,
+    type: "radial-gauge",
+    value: 1000,
+    scale: {
+      min: 0,
+      max: 1300,
+      fill: "#FFFFFF",
       label: {
+        enabled: false,
       },
-      secondaryLabel: {
-            text: 'Kwh',
-            color: '#000000'
+    },
+    targets: [
+      {
+        value: 1050,
+        placement: "outside",
+        shape: "triangle",
+        fill: "#000000",
       },
-      padding: {
-        top: 10,
-        right:30
-      }
-    });
-
+    ],
+    bar: {
+      fill: "#DD702C",
+    },
+    background: {
+      visible: false,
+    },
+    cornerRadius: 99,
+    cornerMode: "container",
+    segmentation: {
+      enabled: false,
+      interval: {
+        count: 4,
+      },
+      spacing: 2,
+    },
+    startAngle: 0,
+    endAngle: 0,
+    label: {},
+    secondaryLabel: {
+      text: "Kwh",
+      color: "#000000",
+    },
+    padding: {
+      top: 10,
+      right: 30,
+    },
+  });
 
   return (
     <StyledWrapper
@@ -65,12 +61,19 @@ function LuminousPowerIndicatorCard() {
       left={"63.1875rem"}
       top={"16.875rem"}
     >
-           <Text       
-      width={"9rem"}
-      left={"6.275rem"}
-      top={"10.975rem"}
-      color={'var(--primaryText)'}> Valor Atual </Text>
-      <AgGauge options={options as any} style={{ width: "17.1875rem", height: "17.1875rem" }} />
+      <Text
+        width={"9rem"}
+        left={"6.275rem"}
+        top={"10.975rem"}
+        color={"var(--primaryText)"}
+      >
+        {" "}
+        Valor Atual{" "}
+      </Text>
+      <AgGauge
+        options={options}
+        style={{ width: "17.1875rem", height: "17.1875rem" }}
+      />
     </StyledWrapper>
   );
 }
--- src/components/MotorPowerCard/index.tsx
+++ src/components/MotorPowerCard/index.tsx
@@ -1,14 +1,61 @@
+import React, { useState } from "react";
+import { AgGauge } from "ag-charts-react";
+import { AgLinearGaugeOptions } from "ag-charts-enterprise";
+
 //Estilo
 import { StyledWrapper } from "./style";
 
 function MotorPowerCard() {
+  const [options, setOptions] = useState<AgLinearGaugeOptions>({
+    type: "linear-gauge",
+    value: 90,
+    scale: {
+      min: 0,
+      max: 100,
+      label: {
+        formatter({ value }) {
+          return `${value.toFixed(0)}%`;
+        },
+      },
+    },
+    thickness: 30,
+    bar: {
+      thickness: 30,
+    },
+    direction: "horizontal",
+    segmentation: {
+      enabled: true,
+      interval: {
+        count: 4,
+      },
+      spacing: 2,
+    },
+    cornerRadius: 99,
+    cornerMode: "container",
+    background: {
+      visible: false,
+    },
+    title: {
+      text: "Potência do Motor",
+    },
+    padding: {
+      top: 10,
+      left: 90,
+    },
+  });
+
   return (
     <StyledWrapper
       width={"65.3125rem"}
       height={"7.5rem"}
       left={"21.5625rem"}
       top={"34.75rem"}
-    ></StyledWrapper>
+    >
+      <AgGauge
+        options={options}
+        style={{ width: "60.1875rem", height: "8.1875rem" }}
+      />
+    </StyledWrapper>
   );
 }
 
