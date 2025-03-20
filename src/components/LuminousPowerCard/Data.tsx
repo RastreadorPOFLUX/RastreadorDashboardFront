2bbbb997f235fbdc7afe8f9ea3449644b766ed05
Guilherme Matera <guimatera@gmail.com>
2025-03-20 10:54:10 -0300

    refactor: Melhoria na compartimentação de arquivos de dados do card de potência luminosa.

---
+++ src/components/LuminousPowerCard/Data.tsx
@@ -0,0 +1,76 @@
+export default function getData() {
+    return [
+      {
+        dateTime:"1-1-2025-15:00:00",
+        hour: "15h",
+        day: 1,
+        month: 1,
+        year: 2025,
+        value: 1300,
+      },
+      {
+        dateTime:"1-1-2025-16:00:00",
+        hour: "16h",
+        day: 1,
+        month: 1,
+        year: 2025,
+        value: 100,
+      },
+      {
+        dateTime:"1-1-2025-17:00:00",
+        hour: "17h",
+        day: 1,
+        month: 1,
+        year: 2025,
+        value: 1100,
+      },
+      {
+      dateTime:"1-1-2025-18:00:00",
+      hour: "18h",
+      day: 1,
+      month: 1,
+      year: 2025,
+      value: 1250,
+      },
+      {
+        dateTime:"1-1-2025-19:00:00",
+        hour: "19h",
+        day: 1,
+        month: 1,
+        year: 2025,
+        value: 1300,
+      },
+      {
+        dateTime:"1-1-2025-20:00:00",
+        hour: "20h",
+        day: 1,
+        month: 1,
+        year: 2025,
+        value: 1000,
+      },
+      {
+        dateTime:"1-1-2025-21:00:00",
+        hour: "21h",
+        day: 1,
+        month: 1,
+        year: 2025,
+        value: 1100,
+      },
+      {
+      dateTime:"1-1-2025-22:00:00",
+      hour: "22h",
+      day: 1,
+      month: 1,
+      year: 2025,
+      value: 1250,
+      },
+      {
+        dateTime:"1-1-2025-23:00:00",
+        hour: "23h",
+        day: 1,
+        month: 1,
+        year: 2025,
+        value: 1300,
+      }
+    ];
+  }
--- src/components/LuminousPowerCard/index.tsx
+++ src/components/LuminousPowerCard/index.tsx
@@ -1,92 +1,15 @@
 import { useState } from "react";
 import { AgCharts } from "ag-charts-react";
 import { AgChartOptions } from "ag-charts-community";
+import getData from "./Data";
 
 //Estilo
 import { StyledWrapper } from "./style";
 
-function getData() {
-  const x = 10;
-  return [
-    {
-      dateTime:"1-1-2025-15:00:00",
-      hour: "15h",
-      day: 1,
-      month: 1,
-      year: 2025,
-      value: 1300,
-    },
-    {
-      dateTime:"1-1-2025-16:00:00",
-      hour: "16h",
-      day: 1,
-      month: 1,
-      year: 2025,
-      value: 100,
-    },
-    {
-      dateTime:"1-1-2025-17:00:00",
-      hour: "17h",
-      day: 1,
-      month: 1,
-      year: 2025,
-      value: 1100,
-    },
-    {
-    dateTime:"1-1-2025-18:00:00",
-    hour: "18h",
-    day: 1,
-    month: 1,
-    year: 2025,
-    value: 1250,
-    },
-    {
-      dateTime:"1-1-2025-19:00:00",
-      hour: "19h",
-      day: 1,
-      month: 1,
-      year: 2025,
-      value: 1300,
-    },
-    {
-      dateTime:"1-1-2025-20:00:00",
-      hour: "20h",
-      day: 1,
-      month: 1,
-      year: 2025,
-      value: 1000,
-    },
-    {
-      dateTime:"1-1-2025-21:00:00",
-      hour: "21h",
-      day: 1,
-      month: 1,
-      year: 2025,
-      value: 1100,
-    },
-    {
-    dateTime:"1-1-2025-22:00:00",
-    hour: "22h",
-    day: 1,
-    month: 1,
-    year: 2025,
-    value: 1250,
-    },
-    {
-      dateTime:"1-1-2025-23:00:00",
-      hour: "23h",
-      day: 1,
-      month: 1,
-      year: 2025,
-      value: 1300,
-    }
-  ];
-}
-
-const intervalTime =  getData()[0].day + "/" + getData()[0].month + "/" + getData()[0].year + " - " +
-getData()[getData().length-1].day + "/" + getData()[getData().length-1].month + "/" + getData()[getData().length-1].year;
-
 function LuminousPowerCard() {
+
+  const intervalTime =  getData()[0].day + "/" + getData()[0].month + "/" + getData()[0].year + " - " +
+  getData()[getData().length-1].day + "/" + getData()[getData().length-1].month + "/" + getData()[getData().length-1].year;
 
   const Average = 1300;
 
