60c0c059ab87675f0450f4c452ea7ff3c850256b
Guilherme Matera <guimatera@gmail.com>
2025-04-11 11:12:38 -0300

    Feature: Criação do indicador do Ângulo da lente.

--- src/components/LensAngleIndicatorCard/style.tsx
+++ src/components/LensAngleIndicatorCard/style.tsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
 import { Wrapper } from "../Cards/style";
+import { Gauge, gaugeClasses } from "@mui/x-charts";
 
 interface Props {
-  width: string;
+  width?: string;
   height?: string;
-  left: string;
-  top: string;
+  left?: string;
+  top?: string;
 }
 
 export const StyledWrapper = styled(Wrapper)<Props>`
@@ -15,4 +16,28 @@ export const StyledWrapper = styled(Wrapper)<Props>`
   left: ${(props) => props.left};
   top: ${(props) => props.top};
   border-radius: 50%;
+`;
+
+export const CircularProgress = styled(Gauge)(() => ({
+  [`& .${gaugeClasses.valueText}`]: {
+    fontSize: 40,
+    fontFamily: "var(--primaryFont)",
+    transform: "translate(8px, 10px)",
+  },
+  [`& .${gaugeClasses.valueArc}`]: {
+    fill: "var(--primaryColor)",
+  },
+  [`& .${gaugeClasses.referenceArc}`]: {},
+}));
+
+export const Text = styled.h3<Props>`
+  color: ${(props) => props.color};
+  text-align: left;
+  font-size: 1.25rem;
+  font-family: var(--primaryFont);
+  font-weight: 500;
+  position: absolute;
+  width: ${(props) => props.width};
+  left: ${(props) => props.left};
+  margin-top: ${(props) => props.top};
 `;
