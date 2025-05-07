3143aa3fcaff6585c98c0213019b93bec6be36a3
Guilherme Matera <guimatera@gmail.com>
2025-05-07 12:05:01 -0300

    Config: Declaração de logos para gráfico de ângulos

--- src/components/AnglesCard/Data.tsx
+++ src/components/AnglesCard/Data.tsx
@@ -1,8 +1,7 @@
 export default function getData() {
-    return {
-      sunPosition: 90,
-      lensAngle: 90,
-      manualSetpoint: 90
-    };
-  }
-  
+  return {
+    sunPosition: 90,
+    lensAngle: 90,
+    manualSetpoint: 170,
+  };
+}
